e985915470ae069822efcd4761a974067c669ff9
Ionut Puiu <yonutbogdan77@gmail.com>
2022-12-13 12:26:22 +0000

    Day 38 OOP ParkingLot Class

---
+++ src/day38_OOP_MethodOverriding/carClass/ParkingLot.java
@@ -0,0 +1,10 @@
+package day38_OOP_MethodOverriding.carClass;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+        Toyota toyota = new Toyota("Camry",2020,20000,"Gray",123456);
+        Tesla tesla = new Tesla("Model S",2022,45000,"Red",15000);
+        BMW bmw = new BMW("X5",2021,100000,"Black",18000);
+
+    }
+}
