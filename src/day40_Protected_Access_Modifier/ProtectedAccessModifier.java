33bf7518c9063991a3ce069a24a229f0dba63842
Ionut Puiu <yonutbogdan77@gmail.com>
2022-12-25 16:54:09 +0200

    Day 40 OOP ProtectedAccessModifier

---
+++ src/day40_Protected_Access_Modifier/ProtectedAccessModifier.java
@@ -0,0 +1,12 @@
+package day40_Protected_Access_Modifier;
+
+public class ProtectedAccessModifier {
+    static  String name1 ="Cydeo";//default
+    protected static String name2 = "WoodenSpoon";//protected
+static void method1(){
+
+}
+protected static void method2(){
+
+}
+}
